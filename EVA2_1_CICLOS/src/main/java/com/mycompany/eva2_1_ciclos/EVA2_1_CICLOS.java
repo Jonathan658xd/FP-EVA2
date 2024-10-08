5ae5d1d7a82233783065fb4c3fa191291bc6afc7
Jonathan Alexis Islas Estrada <L24550378@chihuahua2.tecnm.mx>
2024-10-08 16:50:39 -0600

    Add files via upload

---
+++ EVA2_1_CICLOS/src/main/java/com/mycompany/eva2_1_ciclos/EVA2_1_CICLOS.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_1_ciclos;
+
+/**
+ *
+ * @author ae478
+ */
+public class EVA2_1_CICLOS {
+
+    public static void main(String[] args) {
+        //cilo for
+   
+     
+
+        for(int i = 1; i <= 10; i++){
+            System.out.print(i + " - ");
+            
+           
+        }
+         System.out.println("");   
+        
+        for(int i = 10; i >= 1; i--){
+           System.out.print(i + " - "); 
+        }
+            
+                 
+        
+    }
+}
