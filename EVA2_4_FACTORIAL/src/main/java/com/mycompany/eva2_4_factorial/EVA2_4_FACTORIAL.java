392690dfb37577038a4f1961bdca8dd966424192
Jonathan Alexis Islas Estrada <L24550378@chihuahua2.tecnm.mx>
2024-10-15 16:58:37 -0600

    Add files via upload

---
+++ EVA2_4_FACTORIAL/src/main/java/com/mycompany/eva2_4_factorial/EVA2_4_FACTORIAL.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_4_factorial;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ae478
+ */
+public class EVA2_4_FACTORIAL {
+
+    public static void main(String[] args) {   
+        Scanner captu = new Scanner(System.in);
+        int num;
+        
+        System.out.println("numero");
+        num = captu.nextInt();
+        captu.nextLine();
+        
+        
+    }
+}
