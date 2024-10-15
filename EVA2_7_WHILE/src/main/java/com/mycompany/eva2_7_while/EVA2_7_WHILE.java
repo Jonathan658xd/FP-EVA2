ad8064fa7207cda2bea7a9567d57618a6def3595
Jonathan Alexis Islas Estrada <L24550378@chihuahua2.tecnm.mx>
2024-10-15 16:59:33 -0600

    Add files via upload

---
+++ EVA2_7_WHILE/src/main/java/com/mycompany/eva2_7_while/EVA2_7_WHILE.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_7_while;
+
+/**
+ *
+ * @author ae478
+ */
+public class EVA2_7_WHILE {
+
+    public static void main(String[] args) {
+        //imprimir los numeros de 1 al 10
+        int val = 1;
+        while(val <= 10){   
+            System.out.print(val + "-");
+            val++;
+            
+        }
+        System.out.println("");
+        val = 1;
+        do{
+            System.out.print(val + "-");
+            val++;
+        }while(val <= 10);//REPETIR INTSTRUCCIONES MIENTRAS ESTO SE CUMPLA
+    }
+}
