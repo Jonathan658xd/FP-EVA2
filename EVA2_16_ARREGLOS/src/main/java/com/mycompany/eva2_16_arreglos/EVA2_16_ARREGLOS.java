3bbb47a4732b62bf1164a5b138cf4f77e44bcf7a
Jonathan Alexis Islas Estrada <L24550378@chihuahua2.tecnm.mx>
2024-10-23 14:28:54 -0600

    Add files via upload

---
+++ EVA2_16_ARREGLOS/src/main/java/com/mycompany/eva2_16_arreglos/EVA2_16_ARREGLOS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_16_arreglos;
+
+/**
+ *
+ * @author ae478
+ */
+public class EVA2_16_ARREGLOS {
+
+    public static void main(String[] args) {
+         String[] diasSemana = {"Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"};
+        
+        for (int i = 0; i < diasSemana.length; i++) {
+            System.out.println(diasSemana[i]);
+        }     
+    }
+}
