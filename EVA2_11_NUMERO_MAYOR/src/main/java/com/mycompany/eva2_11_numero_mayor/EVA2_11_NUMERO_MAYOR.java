4d6a0c26ce18d6779a606bfe3b4bba4e379ab8d9
Jonathan Alexis Islas Estrada <L24550378@chihuahua2.tecnm.mx>
2024-10-23 14:27:41 -0600

    Add files via upload

---
+++ EVA2_11_NUMERO_MAYOR/src/main/java/com/mycompany/eva2_11_numero_mayor/EVA2_11_NUMERO_MAYOR.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_11_numero_mayor;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ae478
+ */
+public class EVA2_11_NUMERO_MAYOR {
+
+    public static void main(String[] args) {
+        int num1 = 100;
+        int num2 = 200;
+        for (int i = 0; i <= 2; i++) {
+           String numero;
+        Scanner captu = new Scanner(System.in);
+        do{
+        System.out.println("Esvribe cualquier numero");
+        numero = captu.nextLine();
+       
+        }while(!(numero.equals("-1")));  
+        }
+        for (int i = 0; i >= 1; i++) {
+            
+        }
+        String numero;
+        Scanner captu = new Scanner(System.in);
+        do{
+        System.out.println("Esvribe cualquier numero");
+        numero = captu.nextLine();
+       
+        }while(!(numero.equals("-1")));
+        
+             
+        }
+                 
+    }
+
