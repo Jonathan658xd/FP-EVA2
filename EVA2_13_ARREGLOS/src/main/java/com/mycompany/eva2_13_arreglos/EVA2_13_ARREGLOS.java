244b812eaaa6369fb6977d7bb1e1bd9ddb00b2b6
Jonathan Alexis Islas Estrada <L24550378@chihuahua2.tecnm.mx>
2024-10-23 14:28:07 -0600

    Add files via upload

---
+++ EVA2_13_ARREGLOS/src/main/java/com/mycompany/eva2_13_arreglos/EVA2_13_ARREGLOS.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_13_arreglos;
+
+/**
+ *
+ * @author ae478
+ */
+public class EVA2_13_ARREGLOS {
+
+    public static void main(String[] args) {
+       int[]arreglo = new int[10]; 
+       arreglo[0] = 100;
+       arreglo[9] = 900;
+        System.out.println(arreglo[0]);
+        System.out.println(arreglo[1]);
+        System.out.println(arreglo[2]);
+        System.out.println(arreglo[3]);
+        System.out.println(arreglo[4]);
+        System.out.println(arreglo[5]);
+        System.out.println(arreglo[6]);
+        System.out.println(arreglo[7]);
+        System.out.println(arreglo[8]);
+        System.out.println(arreglo[9]);
+        
+    }
+}
